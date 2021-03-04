3fb563a473edf005ff4fe6d101f8ca666d87eeec
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2021-03-04 21:05:56 +0100

    Update Functions and Methods Homework.py

--- Udemy exercises/Functions and Methods Homework.py
+++ Udemy exercises/Functions and Methods Homework.py
@@ -7,3 +7,26 @@ def vol(radius):
 
 print (vol(2))
 
+
+
+#Write a function that checks whether a number is in a given range (inclusive of high and low)
+
+def ran_check(low,high,num):
+
+    return low <= num <= high
+
+print (ran_check(2,7,5))
+print (ran_check(2,7,7))
+print (ran_check(2,7,20))
+
+
+#Write a Python function that accepts a string and calculates the number of upper case letters and lower case letters.
+
+def up_low(string):
+
+    upper = sum(1 for x in string if x.isupper())
+    lower = sum(1 for x in string if x.islower())
+    return upper, lower
+
+print(up_low('Hello Mr. Rogers, how are you this fine Tuesday?'))
+
