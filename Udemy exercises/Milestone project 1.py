173cee52ca12a3528dcdb0d7f4d06973236087e9
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2021-03-08 12:47:03 +0100

    Update Milestone project 1.py

--- Udemy exercises/Milestone project 1.py
+++ Udemy exercises/Milestone project 1.py
@@ -2,6 +2,7 @@ def clear_screen():
 
     print ('\n'*100)
 
+start_board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
 def display_board(board):
 
@@ -11,9 +12,41 @@ def display_board(board):
         full_line,'\n','  ',board[4],'  |  ',board[5],'  |  ',board[6],'\n',\
            full_line,'\n','  ',board[1],'  |  ',board[2],'  |  ',board[3],sep='')
 
-display_board([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '])
+def player_input():
+    marker = 'wrong'
+    position = 'wrong'
 
-test_board = ['#','X','O','X','O','X','O','X','O','X']
-display_board(test_board)
+    while marker not in ['X','O']:
 
-#display_board([])
+        marker = input('Hi Player 1, do you want to be X or O ?')
+
+        if marker not in ['X','O']:
+            print('Type in O or X')
+
+    while position not in range(1,9):
+
+        position = input('Write your position(1-9): ')
+
+        if position not in range(1,9) and position.isdigit()== False:
+            print('Type in number 1-9')
+        else:
+            position = int(position)
+
+    return marker,position
+
+def place_marker(board, marker, position):
+
+    board[position] = marker
+
+    return board
+
+
+
+display_board(start_board)
+defined_marker,defined_position = player_input()
+board = place_marker(start_board,defined_marker,defined_position)
+display_board(board)
+
+defined_marker,defined_position = player_input()
+board = place_marker(start_board,defined_marker,defined_position)
+display_board(board)
