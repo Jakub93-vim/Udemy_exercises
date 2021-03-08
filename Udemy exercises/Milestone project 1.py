ab13955bc3fd25a314c1e0286612f42cf9c03348
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2021-03-08 21:38:30 +0100

    Update Milestone project 1.py

--- Udemy exercises/Milestone project 1.py
+++ Udemy exercises/Milestone project 1.py
@@ -3,6 +3,8 @@ def clear_screen():
     print ('\n'*100)
 
 start_board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+full_board = ['#','X','X','X','O','X','O','O','O','X']
+win_board = ['#','O','O','O','O','X','O','O','O','X']
 
 def display_board(board): # displays board with board items as a list
 
@@ -12,8 +14,9 @@ def display_board(board): # displays board with board items as a list
         full_line,'\n','  ',board[4],'  |  ',board[5],'  |  ',board[6],'\n',\
            full_line,'\n','  ',board[1],'  |  ',board[2],'  |  ',board[3],sep='')
 
-def player_input():
+def player_input(): # info about what the player wants to be
 
+        print('Welcome to TicTacToe game')
         marker = 'wrong'
 
         while marker not in ['X','O']:
@@ -27,40 +30,125 @@ def player_input():
         else:
             return ('O','X')
 
-def player_choice(board):
+def player_choice(board): # takes players choice and returns the position as a number
 
-    #TODO check if the board is empty on that position
 
     position = 'wrong'
-    while position not in range(1,9):
+    while position not in range(1,10): # ask for position among 1-9
 
         position = input('Write your position(1-9): ')
 
-        if position not in range(1,9) and position.isdigit()== False:
+        if position not in range(1,10) and position.isdigit()== False: # checks if the position is digit
             print('Type in number 1-9')
+        elif space_check(board, int(position)) == True: # checks if the space is free via function space_check
+            print('This position is full, choose another one')
         else:
             position = int(position)
 
     return position
 
-def place_marker(board, marker, position):
+def place_marker(board, marker, position): # puts the marker O or X on the position
 
     board[position] = marker
 
     return board
 
+def full_board_check(board): # checks if the board is full
+    count = 0
+    for ele in board:
+        if ele == 'X' or ele == 'O':
+            count += 1
+    if count == 9:
+        print('End of the game, no winner !!')
+        return False
+    else:
+        return True
+
+def space_check(board, position):
+
+    if board[position] == 'X' or board[position] == 'O':
+        return True
+    else:
+        return False
+
+def win_check(board):
+
+    if board[1] == 'O' and board[2] == 'O' and board[3] == 'O':
+        print('Player O wins !! ')
+        return True
+    elif board[1] == 'O' and board[5] == 'O' and board[9] == 'O':
+        print('Player O wins !! ')
+        return True
+    elif board[1] == 'O' and board[4] == 'O' and board[7] == 'O':
+        print('Player O wins !! ')
+        return True
+    elif board[4] == 'O' and board[5] == 'O' and board[6] == 'O':
+        print('Player O wins !! ')
+        return True
+    elif board[7] == 'O' and board[8] == 'O' and board[9] == 'O':
+        print('Player O wins !! ')
+        return True
+    elif board[7] == 'O' and board[5] == 'O' and board[3] == 'O':
+        print('Player O wins !! ')
+        return True
+    elif board[8] == 'O' and board[5] == 'O' and board[2] == 'O':
+        print('Player O wins !! ')
+        return True
+    elif board[9] == 'O' and board[6] == 'O' and board[3] == 'O':
+        print('Player O wins !! ')
+        return True
+    #player X
+    elif board[1] == 'X' and board[2] == 'X' and board[3] == 'X':
+        print('Player X wins !! ')
+        return True
+    elif board[1] == 'X' and board[5] == 'X' and board[9] == 'X':
+        print('Player X wins !! ')
+        return True
+    elif board[1] == 'X' and board[4] == 'X' and board[7] == 'X':
+        print('Player X wins !! ')
+        return True
+    elif board[4] == 'X' and board[5] == 'X' and board[6] == 'X':
+        print('Player X wins !! ')
+        return True
+    elif board[7] == 'X' and board[8] == 'X' and board[9] == 'X':
+        print('Player X wins !! ')
+        return True
+    elif board[7] == 'X' and board[5] == 'X' and board[3] == 'X':
+        print('Player X wins !! ')
+        return True
+    elif board[8] == 'X' and board[5] == 'X' and board[2] == 'O':
+        print('Player X wins !! ')
+        return True
+    elif board[9] == 'X' and board[6] == 'X' and board[3] == 'O':
+        print('Player X wins !! ')
+        return True
+    else:
+        return False
 
 
-display_board(start_board)
+
 defined_marker = player_input()
+display_board(start_board)
 defined_position = player_choice(start_board)
-board = place_marker(start_board,defined_marker,defined_position)
+board = place_marker(start_board,defined_marker[0],defined_position)
+clear_screen()
 display_board(board)
-n= 1
-while n < 10:
 
-    defined_marker,defined_position = player_input()
+while full_board_check(board):
+    defined_position = player_choice(board)
+    board = place_marker(start_board, defined_marker[1], defined_position)
+    if win_check(board) == True:
+        display_board(board)
+        break
     clear_screen()
-    board = place_marker(start_board,defined_marker,defined_position)
     display_board(board)
-    n+= 1
+
+
+    defined_position = player_choice(board)
+    board = place_marker(start_board, defined_marker[0], defined_position)
+    if win_check(board) == True:
+        display_board(board)
+        break
+    clear_screen()
+    display_board(board)
+
