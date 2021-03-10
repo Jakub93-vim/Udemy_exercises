f06a7029d1c4be1d670bf7d9920aef68ff1e325f
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2021-03-10 19:36:37 +0100

    Update OOP.py

--- Udemy exercises/OOP.py
+++ Udemy exercises/OOP.py
@@ -1,9 +1,57 @@
-class MyClass ():
+class Dog ():
 
-    def __init__(self, myroll):
+    type = 'mammel'
 
-        self.my_atrribute = myroll
+    def __init__(self, breed, name, spots):
 
-my_dog = MyClass(myroll='s maslem')
+        self.breed = breed
+        self.name = name
+        self.spots = spots
 
-print(my_dog.my_atrribute)
+    def bark(self, number):
+        print('Woof Woof, my name is: {} and age {} '.format(self.name, number))
+
+my_dog = Dog(breed='lab',name='Dacan',spots=True)
+
+print(my_dog.bark(5))
+
+class Circle():
+
+    pi = 3.14
+
+    def __init__(self, radius=1):
+
+        self.radius = radius
+
+    def get_circumference(self):
+        return self.radius * self.pi * 2
+
+my_circle = Circle(20)
+
+
+print(my_circle.get_circumference())
+
+class Square():
+
+    def __init__(self, side):
+
+        self.side = side
+
+my_square = Square(side=5)
+
+print(my_square.side)
+
+class Animal():
+
+    def __init__(self):
+        print('Animal created')
+
+    def who_am_i(self):
+        print('I am an animal')
+
+    def eat(self):
+        print('I am eating')
+
+my_animal = Animal()
+
+my_animal.who_am_i()
