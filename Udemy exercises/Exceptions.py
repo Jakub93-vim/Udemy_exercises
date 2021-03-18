23c9cd7f7b6b60a571d21d29ae578c96c6904a35
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2021-03-18 20:13:04 +0100

    Update Exceptions.py

--- Udemy exercises/Exceptions.py
+++ Udemy exercises/Exceptions.py
@@ -1,3 +1,5 @@
+# problem 1
+
 try:
     for i in ['a','b','c']:
         print(i**2)
@@ -7,3 +9,36 @@ except:
 
 finally:
     print('')
+
+#problem 2
+
+try:
+    x = 5
+    y = 0
+
+    z = x/y
+except ZeroDivisionError:
+    print('You divided by zero')
+
+finally:
+    print('All done')
+
+# problem 3
+#Write a function that asks for an integer and prints the square of it.
+# Use a while loop with a try, except, else block to account for incorrect inputs.
+
+def square ():
+
+    while True:
+        try:
+
+            num = (int(input('Give me number: ')))**2
+        except:
+            print('Wrong input')
+            continue
+        else:
+            print(num)
+            break
+
+
+square()
