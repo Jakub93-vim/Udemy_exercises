741d49bb594b18d5169f5d8f6292050074ec1582
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2021-03-20 11:14:39 +0100

    Milestone 2

--- Milestone project 2/Milestone 2.py
+++ Milestone project 2/Milestone 2.py
@@ -1,3 +1,5 @@
+import random
+
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
@@ -15,7 +17,6 @@ class Card:
 
 two_hearts = Card('Hearts','Two')
 
-print(two_hearts)
 
 class Deck:
 
@@ -27,6 +28,42 @@ class Deck:
                 created_card = Card(suit, rank)
                 self.all_cards.append(created_card)
 
-new_deck = Deck()
+    def shuffle(self):
 
-print(new_deck.all_cards[5])
+        random.shuffle(self.all_cards)
+
+    def deal_one(self):
+
+        return self.all_cards.pop()
+
+class Player:
+
+    def __init__(self,name):
+
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        self.all_cards.pop(0)
+
+    def add_cards(self,new_cards):
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+new_deck = Deck()
+new_deck.shuffle()
+mycard = new_deck.deal_one()
+
+print(mycard)
+print(len(new_deck.all_cards))
+
+
+new_player = Player('Jose')
+
+print(new_player)
+
