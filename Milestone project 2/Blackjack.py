0f1d2dc8c1dc00544e88f9c234d4d306603b0301
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2021-03-30 14:47:51 +0200

    Backjack

--- Milestone project 2/Blackjack.py
+++ Milestone project 2/Blackjack.py
@@ -7,6 +7,8 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
          'Queen':10, 'King':10, 'Ace':11}
 
+playing = True
+
 class Card:
 
     def __init__(self,rank,suit):
@@ -15,7 +17,7 @@ class Card:
         self.value = values[rank]
 
     def __str__(self):
-        return f'Card {self.rank} of {self.suit} has a value {self.value}'
+        return f'Card {self.rank} of {self.suit}'
 
 class Player:
 
@@ -45,31 +47,54 @@ class Deck:
                 new_card = Card (rank, suit)
                 self.all_cards.append(new_card)
 
+    def __str__(self):
+        print_deck = ''
+        for card in self.all_cards:
+            print_deck += card.__str__() + '\n'
+        return print_deck
+
     def shuffle(self):
         return random.shuffle(self.all_cards)
 
     def deal_one(self):
         return self.all_cards.pop()
 
-# GAME LOGIC
-print('Welcome to Blackjack, first place a bet and then we can start.\n'
-      '==============')
+class Hand:
+
+    def __init__(self):
+        self.card = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self,card):
+        self.card.append(card)
+
+        self.value = 0
+        for one_card in self.card:
+            self.value += one_card.value
+
+        return self.value
+
+    def adjust_for_ace(self):
+
+        if 'Ace' in self.card and self.value > 21:
+
+
+
 
 my_deck = Deck()
 my_deck.shuffle()
+my_hand = Hand()
 
-player_jack = Player ('Jack')
-player_pc = Player ('PC')
-player_jack.place_bet()
+my_hand.add_card(my_deck.deal_one())
+print(my_hand.value)
+my_hand.add_card(my_deck.deal_one())
+print(my_hand.value)
+my_hand.add_card(my_deck.deal_one())
 
 
-player_jack.take_one(my_deck.deal_one())
-player_jack.take_one(my_deck.deal_one())
-
-print(player_jack.player_cards[0], '\n',player_jack.player_cards[1])
-
-print('Value of the cards is ', player_jack.player_cards[0].value + player_jack.player_cards[1].value )
-
+print(my_hand.card[0],my_hand.card[1],my_hand.card[2])
+print(my_hand.value)
 
 
 
