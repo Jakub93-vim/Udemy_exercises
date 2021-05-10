43eff0313b188679dd2da41ce692bf9d7c6313bf
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2021-05-10 21:44:07 +0200

    Window working

--- Examiner_Project/Database.py
+++ Examiner_Project/Database.py
@@ -10,6 +10,25 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
+def insert_value(spanish_word, english_word):
+    mycursor.execute("INSERT INTO new_vocabulary (spanish, english) VALUES (%s, %s)", (spanish_word, english_word))
+    db.commit()
+    print(f'The word {spanish_word} with translation {english_word} was added.')
+
+def show_vocabulary():
+
+    print ('In the vocabulary are values:', '\n')
+    mycursor.execute("SELECT * FROM new_vocabulary")
+    for x in mycursor.fetchall():
+        print(x)
+
+def return_words():
+
+    mycursor.execute("SELECT * FROM new_vocabulary ORDER BY RAND() LIMIT 1")
+    word = mycursor.fetchone()
+
+    return word[0:2]
+
 #CREATE TABLE
 #mycursor.execute("CREATE TABLE Vocabulary (spanish VARCHAR(50), enslish VARCHAR(50), wordID int PRIMARY KEY AUTO_INCREMENT)")
 
@@ -27,23 +46,3 @@ mycursor = db.cursor()
 #english_word = "friend"
 
 #mycursor.execute("INSERT INTO Vocabulary (spanish, english) VALUES (%s, %s)", (spanish_word, english_word))
-
-
-def insert_value(spanish_word, english_word):
-    mycursor.execute("INSERT INTO Vocabulary (spanish, english) VALUES (%s, %s)", (spanish_word, english_word))
-    db.commit()
-    print(f'The word {spanish_word} with translation {english_word} was added.')
-
-def show_vocabulary():
-
-    print ('In the vocabulary are values:', '\n')
-    mycursor.execute("SELECT * FROM Vocabulary")
-    for x in mycursor.fetchall():
-        print(x)
-
-def return_words():
-
-    mycursor.execute("SELECT * FROM Vocabulary ORDER BY RAND() LIMIT 1")
-    word = mycursor.fetchone()
-
-    return word[0:2]
