b234c36695160df572cf830f2b55907e33b40183
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2021-09-30 16:26:24 +0200

    First working version

--- Milestone project 2/OOP training.py
+++ Milestone project 2/OOP training.py
@@ -1,1 +1,26 @@
-dfg
+import math
+
+class Line:
+
+    def __init__(self, coor1, coor2):
+        pass
+
+    def distance(self):
+
+        dist = math.sqrt((coor1[1]-coor2[1])**2+((coor1[0]-coor2[0])**2))
+
+        return dist
+
+    def slope (self):
+
+        slop = (coor1[1]-coor2[1])/(coor1[0]-coor2[0])
+
+        return slop
+
+coor1 = (5,10)
+coor2 = (7,9)
+
+li = Line (coor1,coor2)
+
+print (li.distance())
+print (li.slope())
